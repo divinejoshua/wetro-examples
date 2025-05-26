0d322f2b9f3560b339d500ded574ae9444bb0fb7
divinejoshua <divineuche399@gmail.com>
2025-05-26 12:44:16 +0100

    Testing wetrocloud data extraction

--- tools.py
+++ tools.py
@@ -6,7 +6,7 @@ client = Wetrocloud(api_key=os.getenv("WETROCLOUD_API_KEY"))
 
 # Extract structured data from a website
 extract_response = client.extract(
-   website="https://www.forbes.com/real-time-billionaires/#7583ee253d78",
-   json_schema=[{"name": "name of rich man", "networth": "amount worth"}]
+   website="https://yeungmancooking.com/collections/ymc-merch",
+   json_schema=[{ "name": "Name of Item", "price": "Price of Item", "image": "Image of Item" }]
 )
 print(extract_response)
